fc675d4a0748e684314ba1fa7d090540c4cdd0dc
Zain-Yousuf <zainyousuf749@gmail.com>
2022-01-20 12:03:44 +0500

    initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+
+  data() {
+    return {
+      msg: "hello world",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
